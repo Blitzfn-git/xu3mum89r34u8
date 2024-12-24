0775bc7a346ed62974f452a9c52b87d6cf9e28a1
Blitzfn-git <jaskubalazs@gmail.com>
2024-12-24 11:09:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
 require('dotenv').config();
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const client = new Client({
-  intents: Object.keys(GatewayIntentBits).map((a) => {
-    return GatewayIntentBits[a];
-  }),
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ],
 });
+
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
+
 app.listen(port, () => {
   console.log(`🔗 Listening to Robin NTW : http://localhost:${port}`);
   console.log(`🔗 Powered By Robinayush`);
 });
 
-
-const statusMessages = ["YouTube: BlitzFN","Twitch: Blitz_Chef","","Website: https://bit.ly/blitzfnweb "];
-
-
+const statusMessages = ["YouTube: BlitzFN", "Twitch: Blitz_Chef", "", "Website: https://bit.ly/blitzfnweb"];
 let currentIndex = 0;
-const channelId = '';
+const channelId = 'YOUR_CHANNEL_ID'; // Replace with your actual channel ID
 
 async function login() {
   try {
@@ -35,26 +36,20 @@ async function login() {
   }
 }
 
-
-
-
 function updateStatusAndSendMessages() {
   const currentStatus = statusMessages[currentIndex];
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'dnd',
+    activities: [{ name: currentStatus, type: ActivityType.Custom }],
+    status: 'online', // Set status to online
   });
 
-  
   const textChannel = client.channels.cache.get(channelId);
-
   if (textChannel instanceof TextChannel) {
-   
     textChannel.send(`Bot status is: ${currentStatus}`);
   } else {
-
+    console.error(`Channel with ID ${channelId} not found or not a valid text channel`);
   }
 
   currentIndex = (currentIndex + 1) % statusMessages.length;
@@ -62,14 +57,12 @@ function updateStatusAndSendMessages() {
 
 client.once('ready', () => {
   console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    💉 Status Changing Code Injected Succesfully`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    💉 Status Changing Code Injected Successfully`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
     updateStatusAndSendMessages();
-  }, 10000);
+  }, 10000); // Change status every 10 seconds
 });
 
 login();
-
-
