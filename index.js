cd896337cd4394e6c7660f75ba4189dcae199717
Blitzfn-git <jaskubalazs@gmail.com>
2024-12-24 10:49:18 +0100

    BlitzBot

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Voxo Studios","Get Started With /help","Your Discord Developement Buddy","voxo01.github.io/bot","Use Code VOXOV30R"];
+const statusMessages = ["YouTube: BlitzFN","Twitch: BLitz_Chef","","Website: https://bit.ly/blitzfnweb "];
 
 
 let currentIndex = 0;
